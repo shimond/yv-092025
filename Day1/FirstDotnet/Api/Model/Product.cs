a82254deb5721908f10fadfc49a14e45c2283a01
Shimon Dahan <shimond@any-techs.co.il>
2025-09-04 12:42:54 +0300

    Refactor Product class to use record type
    Change the Product class to a record type for immutability and value equality. Update properties to use init accessors and remove the constructor in favor of primary constructor syntax. Introduce IEntity interface for common properties and add ProductAsRecord as an alternative representation. Update MyClass to use ProductAsRecord and modify PrintProductName to handle nullable Product, utilizing the with expression for property updates. The IsProductNotNull method remains unchanged but now operates on the new Product record type.

--- Day1/FirstDotnet/Api/Model/Product.cs
+++ Day1/FirstDotnet/Api/Model/Product.cs
@@ -1,20 +1,20 @@
 ﻿namespace Api.Model;
 
-public class Product
+public record Product
 {
-    public int Id { get;}
-    public  string Name { get;  }
-    public decimal Price { get;  }
-    public string? Description { get;  }
-
-    public Product(int id, string name, decimal price, string? description)
-    {
-        Id = id;
-        Name = name;
-        Price = price;
-        Description = description;
-    }
+    public int Id { get; init; }
+    public required string Name { get; init; }
+    public decimal Price { get; init; }
+    public string? Description { get; init; }
 }
+
+public interface IEntity
+{
+    int Id { get; }
+    string Name { get; }
+}
+public record ProductAsRecord(int Id, string Name, decimal Price, string? Description) : IEntity;
+
 
 class MyClass
 {
@@ -24,11 +24,11 @@ class MyClass
         Nullable<int> y = null;
         int? z = null;
 
-        List<Product> products = new List<Product>
+        List<ProductAsRecord> products = new List<ProductAsRecord>
         {
-            new Product { Id = 1, Name = "Product1", Price = 10.0m, Description = "Description1" },
-            new Product { Id = 2, Name = "Product2", Price = 20.0m, Description = "Description2" },
-            new Product { Id = 3, Name = "Product3", Price = 30.0m, Description = null }
+            new ProductAsRecord(1, "Product1", 10.0m, "Description1"),
+            new ProductAsRecord(2, "Product2", 20.0m, "Description2"),
+            new ProductAsRecord(3, "Product3", 30.0m, null)
         };
 
         foreach (var item in products)
@@ -42,34 +42,16 @@ class MyClass
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    void PrintProductName(Product? p)
+    Product? PrintProductName(Product? p)
     {
-        if (p is not null)
+        var p1 = p;
+        if (p1 is not null)
         {
             Console.WriteLine(p.Name);
             Console.WriteLine( p.Price);
-            p.Price = 0;
-
+            p1 = p with { Price = 0 };
         }
+        return p1;
     }
 
 
