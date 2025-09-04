6ac7d592ed6d14b42323f9a51de790f67aeb8c8e
Shimon Dahan <shimond@any-techs.co.il>
2025-09-04 12:26:57 +0300

    Refactor Product class for immutability and clarity
    Change Product properties to read-only with constructor
    initialization to enforce immutability. Update MyClass to
    ensure all products have valid names upon creation.
    Remove unnecessary price check and simplify product
    printing logic with a new PrintProductName method.
    Add IsProductNotNull method to streamline null checks.

--- Day1/FirstDotnet/Api/Model/Product.cs
+++ Day1/FirstDotnet/Api/Model/Product.cs
@@ -2,10 +2,18 @@
 
 public class Product
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public decimal Price { get; set; }
-    public string? Description { get; set; }
+    public int Id { get;}
+    public  string Name { get;  }
+    public decimal Price { get;  }
+    public string? Description { get;  }
+
+    public Product(int id, string name, decimal price, string? description)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+        Description = description;
+    }
 }
 
 class MyClass
@@ -18,20 +26,57 @@ class MyClass
 
         List<Product> products = new List<Product>
         {
-            new Product { Id = 1, Name = null, Price = 10.0m, Description = "Description1" },
+            new Product { Id = 1, Name = "Product1", Price = 10.0m, Description = "Description1" },
             new Product { Id = 2, Name = "Product2", Price = 20.0m, Description = "Description2" },
             new Product { Id = 3, Name = "Product3", Price = 30.0m, Description = null }
         };
 
-        var p = products.FirstOrDefault(p => p.Price > 50);
-        if (IsProductNotNull(p))
+        foreach (var item in products)
         {
-            Console.WriteLine(p!.Name);
+            PrintProductName(item);
+        }
+        Console.WriteLine(  "____________________________");
+        foreach (var item in products)
+        {
+            PrintProductName(item);
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void PrintProductName(Product? p)
+    {
+        if (p is not null)
+        {
+            Console.WriteLine(p.Name);
+            Console.WriteLine( p.Price);
+            p.Price = 0;
+
+        }
+    }
+
+
     bool IsProductNotNull(Product? p)
     {
+    
+        
         return p is not null;
     }
 
