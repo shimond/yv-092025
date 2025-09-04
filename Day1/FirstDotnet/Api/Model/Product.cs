eb12dbeac8d148b0c6d90bc26d7078ab8e4aa8a3
Shimon Dahan <shimond@any-techs.co.il>
2025-09-04 12:21:35 +0300

    Add Product model and MyClass for product management
    Introduce a new namespace `Api.Model` with a `Product` class
    that includes properties for `Id`, `Name`, `Price`, and an
    optional `Description`. Implement `MyClass` to manage a list
    of `Product` instances, demonstrating nullable types and
    providing a method to check for non-null products. The
    constructor also includes logic to find and print products
    with a price greater than 50.

---
+++ Day1/FirstDotnet/Api/Model/Product.cs
@@ -0,0 +1,40 @@
+﻿namespace Api.Model;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+}
+
+class MyClass
+{
+
+    public MyClass()
+    {
+        Nullable<int> y = null;
+        int? z = null;
+
+        List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = null, Price = 10.0m, Description = "Description1" },
+            new Product { Id = 2, Name = "Product2", Price = 20.0m, Description = "Description2" },
+            new Product { Id = 3, Name = "Product3", Price = 30.0m, Description = null }
+        };
+
+        var p = products.FirstOrDefault(p => p.Price > 50);
+        if (IsProductNotNull(p))
+        {
+            Console.WriteLine(p!.Name);
+        }
+    }
+
+    bool IsProductNotNull(Product? p)
+    {
+        return p is not null;
+    }
+
+
+
+}
