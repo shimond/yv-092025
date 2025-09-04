ec182c6911a1c183b0b0226672656eea66d16223
Shimon Dahan <shimond@any-techs.co.il>
2025-09-04 10:33:07 +0300

    Add initial API project setup and middleware
    Create the solution file and project for the new "Api"
    application using .NET 9.0. Include essential files for
    configuration, logging, and middleware handling. Add
    example API requests and define launch settings for
    development environment.

---
+++ Day1/FirstDotnet/Api/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Middlewares;
+
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        await httpContext.Response.WriteAsync("  Middleware 2 A  "); // 2
+        await _next(httpContext);
+        await httpContext.Response.WriteAsync("  Middleware 2 B  "); //5
+    }
+}
+
+public static class LoggingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseLoggingMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<LoggingMiddleware>();
+    }
+}
---
+++ Day1/FirstDotnet/Api/Program.cs
@@ -0,0 +1,40 @@
+using Api.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+// middleware
+app.Use(async (context, next) => {
+
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("  Middleware 1 A  "); // 1
+    if (context.Request.Method.ToUpper() == "POST")
+    {
+        await next();
+    }
+    await context.Response.WriteAsync("  Middleware 1 B  "); // 6
+});
+
+//app.Use(async (context, next) => {
+
+//    await context.Response.WriteAsync("  Middleware 2 A  "); // 2
+//    await next();
+//    await context.Response.WriteAsync("  Middleware 2 B  "); //5
+//});
+// as middleware class
+app.UseLoggingMiddleware();
+
+
+app.Use(async (context, next) => {
+
+    await context.Response.WriteAsync("  Middleware 3 A  "); //3
+    await next();
+    await context.Response.WriteAsync("  Middleware 3 B  "); //4
+});
+
+
+app.Run();
+
+
+// Authentication
