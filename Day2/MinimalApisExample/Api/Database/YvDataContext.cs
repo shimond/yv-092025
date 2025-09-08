505a66d0c2f09df29faa6683e94e695f8df4db37
Shimon Dahan <shimond@any-techs.co.il>
2025-09-08 13:00:50 +0300

    Add Entity Framework Core support and refactor repositories
    Updated `Api.csproj` to include `Microsoft.EntityFrameworkCore`
    version `9.0.8`. Removed `ProductsRepository.cs` to transition
    to a more robust data access layer. Introduced `YvDataContext.cs`
    for Entity Framework Core integration, defining `DbSet` properties
    for `ProductEntity` and `CategoryEntity` with proper relationships.
    Created `EFProductsRepository.cs` to implement the
    `IProductsRepository` interface using EF Core, providing
    asynchronous methods for product retrieval. Modified
    `InMemoryProductsRepository.cs` to maintain in-memory
    functionality while adhering to the new interface.

---
+++ Day2/MinimalApisExample/Api/Database/YvDataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Database;
+
+public class YvDataContext(DbContextOptions<YvDataContext> options) : DbContext(options)
+{
+    public DbSet<ProductEntity> Products => Set<ProductEntity>();
+    public DbSet<CategoryEntity> Categories => Set<CategoryEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<ProductEntity>()
+            .HasOne(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+
+        modelBuilder.Entity<CategoryEntity>()
+            .HasMany(c => c.Products)
+            .WithOne(p => p.Category)
+            .HasForeignKey(p => p.CategoryId);
+    }
+}
---
+++ Day2/MinimalApisExample/Api/Services/EFProductsRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using Api.Contracts;
+using Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Services;
+
+public class EFProductsRepository(YvDataContext dataContext) : IProductsRepository
+{
+    public Task<List<ProductEntity>> GetAllProducts()
+    {
+        var products = dataContext.Products.ToListAsync();
+        return products;
+    }
+
+    public async Task<ProductEntity?> GetProductById(int id)
+    {
+        var product = await dataContext.Products
+            .FirstOrDefaultAsync(p => p.Id == id);  
+
+        return product;
+    }
+
+    public async Task<List<ProductEntity>> GetProductsByCategoryId(int categoryId)
+    {
+        var result = await  dataContext.Products
+            .Where(p => p.CategoryId == categoryId)
+            .ToListAsync();
+
+        return result;
+    }
+}
